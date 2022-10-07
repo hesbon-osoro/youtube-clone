be65a93ccf90d1f52ea9ff200f5998fd28058428
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-07 12:31:00 +0300

    index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux/es/exports';
+import { TypedUseSelectorHook } from 'react-redux/es/types';
+import { AppDispatch, RootState } from '../store';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

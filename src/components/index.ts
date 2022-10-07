a76c916bee16ce8c6906f80afe911426e022dbc6
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-07 17:34:47 +0300

    index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Card } from './Card';
+export { default as WatchCard } from './WatchCard';
+export { default as Spinner } from './Spinner';
+export { default as Navbar } from './Navbar';
+export { default as SearchCard } from './SearchCard';
+export { default as Sidebar } from './Sidebar';

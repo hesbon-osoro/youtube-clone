96213c7b7d93c097e5e1624d3c9fa83adc43e576
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-07 12:29:41 +0300

    index.ts

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,4 @@
+export { getHomePageVideos } from './getHomePageVideos';
+export { getRecommendedVideos } from './getRecommendedVideos';
+export { getSearchPageVideos } from './getSearchPageVideos';
+export { getVideoDetails } from './getVideoDetails';
